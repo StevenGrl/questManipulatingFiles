b7fcc2d04a95641a4dc576261fc65ed561c79070
StevenGrl <steven.grillon.45@gmail.com>
2018-04-20 09:17:14 +0200

    remove recursive directory

--- deleteDirectory.php
+++ deleteDirectory.php
@@ -2,17 +2,23 @@
 
 if (!empty($_GET['name'])) {
     if (is_dir($_GET['name'])) {
-        $nom_repertoire = $_GET['name'];
-        $pointeur = opendir($nom_repertoire);
-        while ($fichier = readdir($pointeur)) {
-            if (($fichier != '.') && ($fichier != '..') && ($fichier != '.DS_Store')) {
-                if (!is_dir($nom_repertoire . '/' . $fichier)) {
-                    unlink($nom_repertoire . '/' .$fichier);
-                }
+        removeDirectory($_GET['name']);
+    }
+}
+
+function removeDirectory($nom_repertoire)
+{
+    $pointeur = opendir($nom_repertoire);
+    while ($fichier = readdir($pointeur)) {
+        if (($fichier != '.') && ($fichier != '..') && ($fichier != '.DS_Store')) {
+            if (is_dir($nom_repertoire . '/' . $fichier)) {
+                removeDirectory($nom_repertoire . '/' . $fichier);
+            } else {
+                unlink($nom_repertoire . '/' .$fichier);
             }
         }
-        rmdir($nom_repertoire);
     }
+    rmdir($nom_repertoire);
 }
 
 header('Location: index.php');
